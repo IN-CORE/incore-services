e7ee5fda91b351c03d3c48b252a029dfcc87123a
Nathan Tolbert <tolbert@illinois.edu>
2017-05-17 15:53:27 -0500

    working run for repo

--- repo/src/main/java/edu/illinois/ncsa/incore/repo/RepoService.java
+++ repo/src/main/java/edu/illinois/ncsa/incore/repo/RepoService.java
@@ -12,6 +12,7 @@ import javax.ws.rs.core.MediaType;
 @Path("/datasets")
 public class RepoService {
     // The Java method will process HTTP GET requests
+    //http://localhost:8080/repo/api/datasets/edu.illinois.ncsa.ergo.eq.buildings.schemas.buildingInventoryVer5.v1.0$Shelby_County_RES31224702005658$converted$all_bldgs_ver5_WGS1984
     @GET
     @Path("{datasetId}")
     @Produces(MediaType.APPLICATION_JSON)
@@ -21,10 +22,15 @@ public class RepoService {
     }
 
     private String formatAsGeoJson(Object dataset) {
+        //TODO: this should return the data in geoJSON format
         return "{\"foo\": \"bar\"}";
     }
 
     private Object loadDataFromRepository(String id) {
+        String urlPart = id.replace("$", "/");
+        String shapefileDatasetUrl = "https://earthquake.ncsa.illinois.edu/ergo-repo/datasets/" + urlPart;
+
+        //TODO: this should load a shapefile from a shapefileDatasetUrl and return some sort of geotools object that represents it (featuredataset or something?)
         return new Object(){
             public String foo = "bar";
         };
