98c9752d47af251890cba7494a7c16fa0a00dbc8
Nathan Tolbert <tolbert@illinois.edu>
2017-05-19 14:28:57 -0500

    repo stub that lets you pass an id and returns geojson

--- repo/src/main/java/edu/illinois/ncsa/incore/repo/RepoService.java
+++ repo/src/main/java/edu/illinois/ncsa/incore/repo/RepoService.java
@@ -1,7 +1,16 @@
 package edu.illinois.ncsa.incore.repo;
 import com.sun.net.httpserver.HttpServer;
 import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import org.apache.commons.io.FilenameUtils;
+import org.geotools.data.shapefile.ShapefileDataStore;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.geojson.feature.FeatureJSON;
+
+import java.io.File;
 import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.file.Files;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.PathParam;
@@ -11,28 +20,57 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/datasets")
 public class RepoService {
-    // The Java method will process HTTP GET requests
+    // The Java method will process HTTP GET requests like the following:
     //http://localhost:8080/repo/api/datasets/edu.illinois.ncsa.ergo.eq.buildings.schemas.buildingInventoryVer5.v1.0$Shelby_County_RES31224702005658$converted$all_bldgs_ver5_WGS1984
     @GET
     @Path("{datasetId}")
     @Produces(MediaType.APPLICATION_JSON)
     public String getDatasetById(@PathParam("datasetId") String id ) {
-        Object dataset = loadDataFromRepository(id);
-        return formatAsGeoJson(dataset);
+        File dataset = null;
+        try {
+            dataset = loadDataFromRepository(id);
+            return formatAsGeoJson(dataset);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "{\"error:\" + \"" + e.getLocalizedMessage() + "\"}";
+        }
     }
 
-    private String formatAsGeoJson(Object dataset) {
+    private String formatAsGeoJson(File shapefile) throws IOException {
         //TODO: this should return the data in geoJSON format
-        return "{\"foo\": \"bar\"}";
+        String geoJson;
+
+        shapefile.setReadOnly();
+
+        ShapefileDataStore store = new ShapefileDataStore(shapefile.toURI().toURL());
+        SimpleFeatureSource source = store.getFeatureSource();
+        SimpleFeatureCollection featureCollection = source.getFeatures();
+        FeatureJSON fjson = new FeatureJSON();
+
+        try (StringWriter writer = new StringWriter()) {
+            fjson.writeFeatureCollection(featureCollection, writer);
+            geoJson = writer.toString();
+        }
+        return geoJson;
     }
 
-    private Object loadDataFromRepository(String id) {
+    private File loadDataFromRepository(String id) throws IOException {
         String urlPart = id.replace("$", "/");
         String shapefileDatasetUrl = "https://earthquake.ncsa.illinois.edu/ergo-repo/datasets/" + urlPart;
+        String baseName = FilenameUtils.getBaseName(shapefileDatasetUrl);
 
-        //TODO: this should load a shapefile from a shapefileDatasetUrl and return some sort of geotools object that represents it (featuredataset or something?)
-        return new Object(){
-            public String foo = "bar";
+        String[] extensionsToGrab = new String[]{
+                "dbf", "prj", "shp", "fix", "qix", "shx"
         };
+
+        String tempDir = Files.createTempDirectory("repo_download_").toString();
+        for (String extension : extensionsToGrab) {
+            HttpDownloader.downloadFile(shapefileDatasetUrl + "." + extension, tempDir);
+        }
+
+        //ok, now the files should be here with the shapefile
+        String shapefile = tempDir + File.separator + baseName + ".shp";
+
+        return new File(shapefile);
     }
 }
