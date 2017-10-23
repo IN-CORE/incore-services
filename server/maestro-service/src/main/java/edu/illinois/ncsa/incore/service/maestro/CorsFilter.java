52d0b928e22c1f0139ae70edcd1cc3880eccb997
indiragp <indiragp@illinois.edu>
2017-10-23 15:41:01 -0500

    Adding Cors Filter

---
+++ server/maestro-service/src/main/java/edu/illinois/ncsa/incore/service/maestro/CorsFilter.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2017 University of Illinois and others.  All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the BSD-3-Clause which accompanies this distribution,
+ * and is available at https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Contributors:
+ * Omar Elabd, Nathan Tolbert
+ */
+
+package edu.illinois.ncsa.incore.service.maestro;
+
+    import javax.ws.rs.container.ContainerRequestContext;
+    import javax.ws.rs.container.ContainerResponseContext;
+    import javax.ws.rs.container.ContainerResponseFilter;
+    import javax.ws.rs.core.MultivaluedMap;
+    import java.io.IOException;
+
+public class CorsFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+
+        headers.add("Access-Control-Allow-Origin", "*");
+        headers.add("Access-Control-Allow-Methods", "GET");
+    }
+}
