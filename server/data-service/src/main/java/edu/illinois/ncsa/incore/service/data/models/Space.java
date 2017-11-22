ef21b358e1cfb5951855c4088b24da4253d78b55
Jong Lee <jonglee1@illinois.edu>
2017-11-22 08:57:51 -0600

    renamed model to models

---
+++ server/data-service/src/main/java/edu/illinois/ncsa/incore/service/data/models/Space.java
@@ -0,0 +1,59 @@
+/*
+ * ******************************************************************************
+ *   Copyright (c) 2017 University of Illinois and others.  All rights reserved.
+ *   This program and the accompanying materials are made available under the
+ *   terms of the BSD-3-Clause which accompanies this distribution,
+ *   and is available at https://opensource.org/licenses/BSD-3-Clause
+ *
+ *   Contributors:
+ *   Yong Wook Kim (NCSA) - initial API and implementation
+ *  ******************************************************************************
+ */
+
+package edu.illinois.ncsa.incore.service.data.models;
+
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.annotations.Property;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+/**
+ * Created by ywkim on 10/2/2017.
+ */
+
+@XmlRootElement
+public class Space {
+    @Id
+    @Property("_id")
+    private ObjectId id = new ObjectId();
+
+    private String name = null;
+    private List<String> datasetIds = null;
+
+    public String getId() {
+        return id.toString();
+    }
+    public void setId(String id) {
+        this.id = new ObjectId(id);
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getDatasetIds() {
+        return datasetIds;
+    }
+    public void setDatasetIds(List<String> datasetIds) {this.datasetIds = datasetIds;}
+
+    public void addDatasetId(String id) {
+        if (id != null) {
+            getDatasetIds().add(id);
+        }
+    }
+}
---
+++ server/data-service/src/main/java/edu/illinois/ncsa/incore/service/data/models/mvz/Property.java
@@ -0,0 +1,51 @@
+/*
+ * ******************************************************************************
+ *   Copyright (c) 2017 University of Illinois and others.  All rights reserved.
+ *   This program and the accompanying materials are made available under the
+ *   terms of the BSD-3-Clause which accompanies this distribution,
+ *   and is available at https://opensource.org/licenses/BSD-3-Clause
+ *
+ *   Contributors:
+ *   Yong Wook Kim (NCSA) - initial API and implementation
+ *  ******************************************************************************
+ */
+
+package edu.illinois.ncsa.incore.service.data.models.mvz;
+
+/**
+ * Created by ywkim on 7/31/2017.
+ */
+public class Property {
+    private String name;
+    private String value;
+    private String type;
+    private String category;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
