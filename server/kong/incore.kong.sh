a87e3da83557178ddd1c71b16813565f3ebea151
Nathan Tolbert <tolbert@illinois.edu>
2017-11-29 16:07:39 -0600

    fix kong ports

--- server/kong/incore.kong.sh
+++ server/kong/incore.kong.sh
@@ -1,14 +1,10 @@
 #add the apis that are forwarded by kong
-<<<<<<< HEAD
 curl -i -X DELETE --url http://localhost:8001/apis/fragility
-=======
->>>>>>> master
 curl -i -X POST \
   --url http://localhost:8001/apis/ \
   --data 'name=fragility' \
   --data 'uris=/fragility' \
-<<<<<<< HEAD
-  --data 'upstream_url=http://localhost:8888/fragility/'
+  --data 'upstream_url=http://localhost:8080/fragility/'
 
 
 
@@ -17,7 +13,7 @@ curl -i -X POST \
   --url http://localhost:8001/apis/ \
   --data 'name=maestro' \
   --data 'uris=/maestro' \
-  --data 'upstream_url=http://localhost:8888/maestro/'
+  --data 'upstream_url=http://localhost:8080/maestro/'
 
 
 
@@ -28,7 +24,7 @@ curl -i -X POST \
   --url http://localhost:8001/apis/ \
   --data 'name=repo' \
   --data 'uris=/repo' \
-  --data 'upstream_url=http://localhost:8888/repo/'
+  --data 'upstream_url=http://localhost:8080/repo/'
 
 
 
@@ -40,7 +36,7 @@ curl -i -X POST \
   --url http://localhost:8001/apis/ \
   --data 'name=hazard' \
   --data 'uris=/hazard' \
-  --data 'upstream_url=http://localhost:8888/hazard/'
+  --data 'upstream_url=http://localhost:8080/hazard/'
 
 
 #create an anonyous user
@@ -63,55 +59,5 @@ curl -i -X POST \
   --data 'config.attribute=uid' \
   --data 'config.ldaps=true' \
   --data "config.anonymous=$ANON_ID" 
-  
-=======
-  --data 'upstream_url=http://localhost:8080/fragility/'
-
-curl -i -X POST \
-  --url http://localhost:8001/apis/ \
-  --data 'name=maestro' \
-  --data 'uris=/maestro' \
-  --data 'upstream_url=http://localhost:8080/maestro/'
-
-curl -i -X POST \
-  --url http://localhost:8001/apis/ \
-  --data 'name=repo' \
-  --data 'uris=/repo' \
-  --data 'upstream_url=http://localhost:8080/repo/'
-
-curl -i -X POST \
-  --url http://localhost:8001/apis/ \
-  --data 'name=hazard' \
-  --data 'uris=/hazard' \
-  --data 'upstream_url=http://localhost:8080/hazard/'
-
-curl -i -X POST \
-  --url http://localhost:8001/apis/ \
-  --data 'name=auth' \
-  --data 'uris=/auth' \
-  --data 'upstream_url=http://localhost:8080/auth/'
 
 
-#create an anonyous user
-curl -i -X POST \
-  --url http://localhost:8001/consumers/ \
-  --data 'username=anonymous' 
-
-#get the anonymous user id
-ANON_JSON=`curl -X GET  --url http://localhost:8001/consumers/anonymous`
-ANON_ID=`echo "$ANON_JSON" | sed -e 's/.*"id":"\(.*\)"}/\1/g'`
-
-#this requires that my ldaps patch has been applied to /usr/local/share/lua/xx/kong/
-curl -i -X POST \
-  --url http://localhost:8001/plugins/ \
-  --data 'name=ldap-auth' \
-  --data 'config.ldap_port=636' \
-  --data 'config.anonymous=XXX' \
-  --data 'config.base_dn=dc=ncsa,dc=illinois,dc=edu' \
-  --data 'config.ldap_host=ldap.ncsa.illinois.edu' \
-  --data 'config.attribute=uid' \
-  --data 'config.ldaps=true' \
-  --data "config.anonymous=$ANON_ID" 
-  
-
->>>>>>> master
