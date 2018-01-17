bb6474fd3c349a9fc3961c5264295a790e5965b1
Nathan Tolbert <tolbert@illinois.edu>
2018-01-17 15:25:50 -0600

    use openjdk instead of oracle

--- vagrant/vagrant-bootstrap.sh
+++ vagrant/vagrant-bootstrap.sh
@@ -1,12 +1,7 @@
 #install oracle jdk 8
 apt-get update
 apt-get install -y software-properties-common python-software-properties
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true |  /usr/bin/debconf-set-selections
-add-apt-repository ppa:webupd8team/java -y
-apt-get update
-apt-get install -y oracle-java8-installer
-echo "Setting environment variables for Java 8.."
-apt-get install -y oracle-java8-set-default
+apt-get install -y openjdk-8-jdk
 apt-get install -y mongodb mongodb-clients
 apt-get install -y unzip
 
