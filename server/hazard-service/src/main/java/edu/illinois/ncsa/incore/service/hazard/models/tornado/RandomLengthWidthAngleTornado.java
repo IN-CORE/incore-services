bf2965c506d07475543ae72d6856d5815854e3a1
Chen Wang <cwang138@illinois.edu>
2018-07-19 11:28:40 -0500

    remove unused library; add copyright headers

--- server/hazard-service/src/main/java/edu/illinois/ncsa/incore/service/hazard/models/tornado/RandomLengthWidthAngleTornado.java
+++ server/hazard-service/src/main/java/edu/illinois/ncsa/incore/service/hazard/models/tornado/RandomLengthWidthAngleTornado.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2017 University of Illinois and others.  All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the BSD-3-Clause which accompanies this distribution,
+ * and is available at https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Contributors:
+ * Chris Navarro, Chen Wang (NCSA) - initial API and implementation
+ *******************************************************************************/
 package edu.illinois.ncsa.incore.service.hazard.models.tornado;
 
 import edu.illinois.ncsa.incore.service.hazard.models.tornado.utils.TornadoUtils;
--- server/hazard-service/src/main/java/edu/illinois/ncsa/incore/service/hazard/models/tornado/utils/TornadoUtils.java
+++ server/hazard-service/src/main/java/edu/illinois/ncsa/incore/service/hazard/models/tornado/utils/TornadoUtils.java
@@ -39,7 +39,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
 import java.util.ArrayList;
 import java.util.List;
 
