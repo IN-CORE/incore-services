6d0ac1c0cdba2f1733d616f57dad83b9d1506829
vnarah2 <vnarah@illinois.edu>
2018-09-05 16:41:40 -0500

    WIP: INCORE-490: Need to add cores calculation

--- server/hazard-service/src/main/java/edu/illinois/ncsa/incore/service/hazard/controllers/HurricaneController.java
+++ server/hazard-service/src/main/java/edu/illinois/ncsa/incore/service/hazard/controllers/HurricaneController.java
@@ -9,6 +9,7 @@
  *******************************************************************************/
 package edu.illinois.ncsa.incore.service.hazard.controllers;
 
+import com.mongodb.util.JSON;
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.Point;
@@ -23,9 +24,11 @@ import edu.illinois.ncsa.incore.service.hazard.models.hurricane.Hurricane;
 //import edu.illinois.ncsa.incore.service.hazard.models.tornado.utils.TornadoUtils;
 //import edu.illinois.ncsa.incore.service.hazard.utils.ServiceUtil;
 import edu.illinois.ncsa.incore.service.hazard.models.eq.types.IncorePoint;
+import edu.illinois.ncsa.incore.service.hazard.models.hurricane.HurricaneGrid;
 import org.apache.log4j.Logger;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.feature.DefaultFeatureCollection;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject; //TODO: org.json.* vs org.json.simple.*
 // http://www.rojotek.com/blog/2009/05/07/a-review-of-5-java-json-libraries/
 
@@ -36,7 +39,10 @@ import javax.ws.rs.core.Response;
 import java.io.File;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
 
 import org.apache.commons.math3.complex.Complex;
 import org.apache.commons.math3.complex.ComplexFormat;
@@ -60,6 +66,7 @@ public class HurricaneController {
                                              @QueryParam("TransD") int transD, @QueryParam("LandfallLoc") String landfallLoc,
                                              @QueryParam("model") String model,
                                              @DefaultValue("6") @QueryParam("resolution") int resolution) {
+        //TODO: Can resolution be double? It's being hardcoded in Grid calculation.
 
         //This function simulates wind fields for the selected data-driven model
 
@@ -67,12 +74,13 @@ public class HurricaneController {
         //getHurricaneByModel(model)
         JSONObject params = hurricane.getHurricaneParameters();
 
-        ArrayList<String> Vt0New = (ArrayList<String>) params.get("VTs_o_new");
+        // May be it's better to use List
+        List<String> Vt0New = (List<String>) params.get("VTs_o_new");
         long longLandfall = (long)params.get("index_landfall");
 
         int indexLandfall = toIntExact(longLandfall) - 1 ; //Converting mat index to java
         IncorePoint landfallPoint = new IncorePoint(landfallLoc);
-        ArrayList<Double> timeNewRadii = (ArrayList<Double>) params.get("time_radii_new");
+        List<Double> timeNewRadii = (List<Double>) params.get("time_radii_new");
 
         ComplexFormat cf = new ComplexFormat("j");
         Complex vt0 =  cf.parse(Vt0New.get(0));
@@ -89,7 +97,7 @@ public class HurricaneController {
 
         double th = atan2(x1*y2-y1*x2,x1*x2+y1*y2);
 
-        ArrayList<Complex> VTsSimu = new ArrayList<Complex>();
+        List<Complex> VTsSimu = new ArrayList<Complex>();
 
         for(String s: Vt0New){
             Complex c1 = cf.parse(s);
@@ -99,11 +107,26 @@ public class HurricaneController {
             VTsSimu.add(polar(abNew, angle+th));
         }
 
-        ArrayList<IncorePoint> track = locateNewTrack(timeNewRadii ,VTsSimu, landfallPoint, indexLandfall);
-        int j= 1;
+        List<IncorePoint> track = locateNewTrack(timeNewRadii ,VTsSimu, landfallPoint, indexLandfall);
 
+        JSONArray para = (JSONArray) params.get("para");
+        JSONArray omegaFitted = (JSONArray) params.get("omega_miss_fitted");
+        JSONArray radiusM = (JSONArray) params.get("rconv");
+        JSONArray zonesFitted = (JSONArray) params.get("contouraxis_zones_fitted");
+
+        int paras = para.size();
+
+        ArrayList<HurricaneGrid> grids = new ArrayList<HurricaneGrid>();
+
+        for(int i=0; i<paras; i++){
+            HurricaneGrid hgrid = defineGrid(track.get(i), resolution);
+            simulateWindfieldWithCores((JSONObject) para.get(i), hgrid, VTsSimu.get(i),
+                (JSONArray) omegaFitted.get(i), (JSONArray) zonesFitted.get(i), radiusM);
 
         }
+        int p=0;
+
+    }
 
     //TODO: Move all these functions to HurricaneUtils
 
@@ -111,25 +134,36 @@ public class HurricaneController {
         return (new Complex(rho * Math.cos(theta), rho * Math.sin(theta)));
     }
 
-    private static ArrayList<IncorePoint> locateNewTrack(ArrayList<Double> timeNewRadii ,ArrayList<Complex> VTsSimu, IncorePoint LandfallLoc, int indexLandfall){
+    private static List<IncorePoint> locateNewTrack(List<Double> timeNewRadii ,List<Complex> VTsSimu, IncorePoint LandfallLoc, int indexLandfall){
 
-        ArrayList<IncorePoint> centers = new ArrayList<IncorePoint>(indexLandfall+1);
+        List<IncorePoint> centers = new ArrayList<IncorePoint>(indexLandfall+1);
 
-       //centers.
+        int timesSize = 0;
+        timesSize = timeNewRadii.size();
+
+        List<IncorePoint> centersSimu = new ArrayList<IncorePoint>(timesSize);
 
         centers.add(LandfallLoc);
 
         for(int i=indexLandfall; i >= 1 ; i--){
             double deltaT = 0;
             deltaT = (timeNewRadii.get(i) - timeNewRadii.get(i-1))* 3600;
-            Complex deltaDist = VTsSimu.get(i).multiply(deltaT);
-            centers.add(0, convertXY2LongLati(centers.get(0), deltaDist, "hwind"));
+            Complex deltaDist = VTsSimu.get(i-1).multiply(deltaT);
+            centers.add(0, convertXY2LongLati(centers.get(0), deltaDist, true, "hwind"));
         }
 
-        return centers;
+        centersSimu.add(centers.get(0));
+
+        for(int i = 1; i < timesSize; i++){
+            double deltaT = 0;
+            deltaT = (timeNewRadii.get(i) - timeNewRadii.get(i-1))* 3600;
+            Complex deltaDist = VTsSimu.get(i-1).multiply(deltaT);
+            centersSimu.add(convertXY2LongLati(centersSimu.get(i-1), deltaDist, false, "hwind"));
+        }
+        return centersSimu;
     }
 
-    private static IncorePoint convertXY2LongLati(IncorePoint center, Complex deltaDist, String method){
+    private static IncorePoint convertXY2LongLati(IncorePoint center, Complex deltaDist, boolean backwards, String method){
         final double R =6371*1000; // mean radius of earth, meter
         final double para = 3.5;
 
@@ -138,6 +172,11 @@ public class HurricaneController {
 
         double x = deltaDist.getReal();
         double y = deltaDist.getImaginary();
+
+        if(backwards){
+            x = -x;
+            y = -y;
+        }
 
         double c = abs(y)/R;
         double a = pow(tan(c/2), 2)/(1+pow((tan(c/2)),2));
@@ -153,6 +192,9 @@ public class HurricaneController {
         double deltaLong = 0;
         double longi = 0;
 
+        // TODO:Precision difference of .001 in mat lab vs java calculation. Due to default rounding that was applied.
+        //Only in long calculation.
+        //Ex. -79.2014 from matlab is calculated as -79.202741231313
         if(method == "hwind"){
              deltaLong = 2 * asin(sqrt(a1/(cos(para/(Math.PI*180))))) / Math.PI*180;
              longi = centerLong + deltaLong*signum(x);
@@ -170,10 +212,111 @@ public class HurricaneController {
         return new IncorePoint(lati+","+longi);
     }
 
-    private static IncorePoint convertXY2LongLati( IncorePoint center, Complex deltaDist){
-        return convertXY2LongLati(center,deltaDist, "hwind");
+    private static IncorePoint convertXY2LongLati( IncorePoint center, Complex deltaDist, boolean backwards){
+        return convertXY2LongLati(center,deltaDist, backwards,"hwind");
     }
 
+    private static HurricaneGrid defineGrid(IncorePoint center, int resolution){
+
+        HurricaneGrid g = new HurricaneGrid();
+        double centerLong = center.getLocation().getX();
+        double centerLati = center.getLocation().getY();
+
+        double xMax = 6 * 80; //TODO: Should 6 come from resolution?
+        //double xM = xMax*1000;
+
+       List<Double> xsIntermediate = new ArrayList<Double>();
+       List<Double> xs = new ArrayList<Double>();
+       List<Double> ys = new ArrayList<Double>();
+
+        int i = 0;
+        for(double x= -xMax; x <= xMax;x= x+resolution){
+            xsIntermediate.add(x);
+            i++;
+        }
+        int cords = xsIntermediate.size();
+
+        for(int j=0; j < cords; j++ ){
+            for(int k=0; k< cords; k++) {
+                xs.add(xsIntermediate.get(j)*1000);
+                ys.add(xsIntermediate.get(k)*1000);
+            }
+        }
+
+        List<Double> latis = new ArrayList<Double>();
+        List<Double> longis = new ArrayList<Double>();
+
+        int xyCords = xs.size();
+
+        for(int j=0; j< xyCords; j++){
+            IncorePoint p = convertXY2LongLati(center, new Complex(xs.get(j), ys.get(j)), false);
+            latis.add(p.getLocation().getY());
+            longis.add(p.getLocation().getX());
+        }
+
+        List<Double> lati = new ArrayList<Double>();
+        List<Double> longi = new ArrayList<Double>();
+
+        for(int j=0; j<cords;j++){
+            lati.add(latis.get(j));
+            longi.add(longis.get(j*cords));
+        }
+
+
+        g.setXs(xs);
+        g.setYs(ys);
+        g.setLongis(longis);
+        g.setLatis(latis);
+        g.setLati(lati);
+        g.setLongi(longi);
+        g.setCenter(center);
+        return g;
+    }
+
+    private static void simulateWindfieldWithCores(JSONObject para, HurricaneGrid grid, Complex VTs,
+                                                    JSONArray omegaFitted, JSONArray zonesFitted, JSONArray radiusM){
+        //Add functionaility to Access Maps
+        double rho = 1.2754; // air density, kg/m^3
+        double mb2Pa = 100; // unit change from mb to Pa
+        double pn = 1013.25*mb2Pa; // Pa, ambient pressure, 101.325 kPa, 29.921 inHg, 760 mmHg.
+        double kt2ms = 0.514444; // unit change from KT to m/s
+        double fr = 0.8; //Conversion parameter
+
+        ArrayList<Double> thetaRadians = new ArrayList<Double>();
+
+        for(double i=0; i <=360; i = i+ 0.1){
+            thetaRadians.add(i*Math.PI/180);
+        }
+
+        double bInner = (double)para.get("b_inner");
+        double bOuter = (double)para.get("b_outer");
+        double fCorolosis = (double)para.get("f");
+
+        JSONArray rmThetaVspInner = (JSONArray) para.get("rm_theta_vsp_inner");
+        JSONArray rmThetaVspOuter = (JSONArray) para.get("rm_theta_vsp_outer");
+        double pc = (double)para.get("pc"); //central Pressure
+        JSONArray vgInner = (JSONArray) para.get("vg_inner");
+        JSONArray vgOuter = (JSONArray) para.get("vg_outer");
+
+        double rm = (double)para.get("rm_outer"); // inner is not being used at all. why?
+
+        /*
+        Convert Cartesian Coordinates to Polar Coordinates
+         */
+        List<Double> xs = grid.getXs();
+        List<Double> ys = grid.getYs();
+
+        int cordSize = xs.size();
+
+        List<Double> thetas = new ArrayList<Double>();
+        List<Double> r = new ArrayList<Double>();
+
+        for(int i=0; i< cordSize; i++){
+            double th = atan2(xs.get(i), ys.get(i));
+            double rx = hypot(xs.get(i), ys.get(i));
+        }
+
+    }
 
 
 }
