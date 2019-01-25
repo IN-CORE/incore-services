d916d17d602ac26351b0a373c21ccc3762eefaa0
Yong Wook Kim <ywkim@illinois.edu>
2019-01-25 14:17:28 -0600

    remove unnecessary file

---
+++ server/hazard-service/src/main/java/edu/illinois/ncsa/incore/service/hazard/models/eq/FaultMechanism.java
@@ -0,0 +1,10 @@
+package edu.illinois.ncsa.incore.service.hazard.models.eq;
+
+public class FaultMechanism {
+    public static final String STRIKE_SLIP = "Strike-Slip";
+    public static final String REVERSE_THRUST = "Reverse-Thrust";
+    public static final String REVERSE_SLIP = "Reverse-Slip";
+    public static final String NORMAL = "Normal";
+    public static final String ALL = "All";
+    public static final String REVERSE = "Reverse";
+}
