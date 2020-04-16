77a8f688490af3d94a01f71d41280db7a505412f
Chen Wang <cwang138@illinois.edu>
2020-04-16 13:41:23 -0500

    add parametric fragility curve

---
+++ server/dfr3-service/src/main/java/edu/illinois/ncsa/incore/service/dfr3/models/ParametricFragilityCurve.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2019 University of Illinois and others.  All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Mozilla Public License v2.0 which accompanies this distribution,
+ * and is available at https://www.mozilla.org/en-US/MPL/2.0/
+ *
+ * Contributors:
+ * Christopher Navarro, Chen Wang
+ *******************************************************************************/
+
+package edu.illinois.ncsa.incore.service.dfr3.models;
+
+import java.util.List;
+
+public class ParametricFragilityCurve extends FragilityCurve {
+    public List<CurveParameter> parameters;
+    public CurveType curveType;
+
+
+    public ParametricFragilityCurve() {
+        super();
+    }
+
+    public ParametricFragilityCurve(List<CurveParameter> parameters, CurveType curveType, String label) {
+        super(label);
+
+        this.parameters = parameters;
+        this.curveType = curveType;
+    }
+}
