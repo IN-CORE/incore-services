93bb8774243b92f0ab2ee7447be48aee7f7ee3d9
vnarah2 <vnarah2@illinois.edu>
2020-05-11 00:33:12 -0500

    INCORE1-560: Fixed a bug in dataservice to only attempt deleting data from geoserver when the GEOSERVER_ENABLE property is set

--- server/data-service/src/main/java/edu/illinois/ncsa/incore/service/data/controllers/DatasetController.java
+++ server/data-service/src/main/java/edu/illinois/ncsa/incore/service/data/controllers/DatasetController.java
@@ -398,15 +398,16 @@ public class DatasetController {
                     }
                 }
                 // remove geoserver layer
-                if (format.equalsIgnoreCase(FileUtils.FORMAT_NETWORK)) {
-                    // remove network dataset
-                    boolean linkRemoved = GeoserverUtils.removeLayerFromGeoserver(datasetId, "_link");
-                    boolean nodeRemoved = GeoserverUtils.removeLayerFromGeoserver(datasetId, "_node");
-                    boolean storeRemoved = GeoserverUtils.removeStoreFromGeoserver(datasetId);
-                } else {
-                    boolean layerRemoved = GeoserverUtils.removeLayerFromGeoserver(datasetId);
+                if (GEOSERVER_ENABLE.equalsIgnoreCase("true")) {
+                    if (format.equalsIgnoreCase(FileUtils.FORMAT_NETWORK)) {
+                        // remove network dataset
+                        boolean linkRemoved = GeoserverUtils.removeLayerFromGeoserver(datasetId, "_link");
+                        boolean nodeRemoved = GeoserverUtils.removeLayerFromGeoserver(datasetId, "_node");
+                        boolean storeRemoved = GeoserverUtils.removeStoreFromGeoserver(datasetId);
+                    } else {
+                        boolean layerRemoved = GeoserverUtils.removeLayerFromGeoserver(datasetId);
+                    }
                 }
-
             }
         } else {
             throw new IncoreHTTPException(Response.Status.FORBIDDEN, this.username + " is not authorized to delete the dataset " +datasetId);
