c47fa27dbdc906a8a49c60c703ce0442e5b5a7b1
Chen Wang <cwang138@illinois.edu>
2020-09-22 14:32:08 -0500

    initialize new models for new fragility

---
+++ server/dfr3-service/src/main/java/edu/illinois/ncsa/incore/service/dfr3/newmodels/DFR3Set.java
@@ -0,0 +1,83 @@
+package edu.illinois.ncsa.incore.service.dfr3.newmodels;
+
+import edu.illinois.ncsa.incore.common.auth.Privileges;
+import org.bson.types.ObjectId;
+import dev.morphia.annotations.Id;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@XmlRootElement
+public abstract class DFR3Set {
+    @Id
+    protected ObjectId id;
+
+    @XmlTransient
+    protected String legacyId;
+
+    protected String description;
+
+    protected List<String> authors = new ArrayList<>();
+    protected PaperReference paperReference;
+
+    protected String resultUnit;
+    protected String resultType;
+
+
+    protected String hazardType;
+    protected String inventoryType;
+
+    protected String creator;
+
+    // region Getters
+    public String getId() {
+        if (id == null) {
+            return null;
+        } else {
+            return id.toHexString();
+        }
+    }
+
+    public String getLegacyId() {
+        return legacyId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public PaperReference getPaperReference() {
+        return paperReference;
+    }
+
+    public String getResultUnit() {
+        return resultUnit;
+    }
+
+    public String getResultType() {
+        return resultType;
+    }
+
+    public String getHazardType() {
+        return hazardType;
+    }
+
+    public String getInventoryType() {
+        return inventoryType;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+}
