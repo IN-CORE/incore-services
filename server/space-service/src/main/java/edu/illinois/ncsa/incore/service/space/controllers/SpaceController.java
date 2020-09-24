caadd2213998e6d566d9889901e7c2d0a5116c57
Diego Calderon <diegoac2@illinois.edu>
2020-09-24 14:08:27 -0500

    added log statement to constructor

--- server/space-service/src/main/java/edu/illinois/ncsa/incore/service/space/controllers/SpaceController.java
+++ server/space-service/src/main/java/edu/illinois/ncsa/incore/service/space/controllers/SpaceController.java
@@ -62,7 +62,7 @@ import java.util.*;
 
 @Path("spaces")
 public class SpaceController {
-    private static final String DATA_SERVICES_URL = System.getenv("DATA_SERVICE_URL");
+    private static final String DATA_SERVICE_URL = System.getenv("DATA_SERVICE_URL");
     private static final String HAZARD_SERVICE_URL = System.getenv("HAZARD_SERVICE_URL");
     private static final String DFR3_SERVICE_URL = System.getenv("DFR3_SERVICE_URL");
     private static final String EARTHQUAKE_URL =  HAZARD_SERVICE_URL + "/hazard/api/earthquakes/";
@@ -70,10 +70,10 @@ public class SpaceController {
     private static final String HURRICANE_URL = HAZARD_SERVICE_URL + "/hazard/api/hurricaneWindfields/";
     private static final String TSUNAMI_URL = HAZARD_SERVICE_URL + "/hazard/api/tsunamis/";
     private static final String FRAGILITY_URL = DFR3_SERVICE_URL + "/dfr3/api/fragilities/";
-    private static final String REPAIR_URL =DFR3_SERVICE_URL + "/dfr3/api/repairs/";
+    private static final String REPAIR_URL = DFR3_SERVICE_URL + "/dfr3/api/repairs/";
     private static final String RESTORATION_URL = DFR3_SERVICE_URL + "/dfr3/api/restorations/";
     private static final String MAPPING_URL = DFR3_SERVICE_URL + "/dfr3/api/mappings/";
-    private static final String DATA_URL = DATA_SERVICES_URL + "/data/api/datasets/";
+    private static final String DATA_URL = DATA_SERVICE_URL + "/data/api/datasets/";
 
     private static final String SPACE_MEMBERS = "members";
     private static final String SPACE_METADATA = "metadata";
@@ -92,6 +92,9 @@ public class SpaceController {
     public SpaceController(
         @ApiParam(value = "User credentials.", required = true) @HeaderParam("x-auth-userinfo") String userInfo) {
         this.username = UserInfoUtils.getUsername(userInfo);
+        logger.warn(HAZARD_SERVICE_URL);
+        logger.warn(DATA_SERVICE_URL);
+        logger.warn(DFR3_SERVICE_URL);
     }
 
     @POST
