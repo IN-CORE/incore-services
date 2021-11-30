7322d457caed3df318abb473ad9b92bf46571a75
Chen Wang <cwang138@illinois.edu>
2021-11-30 17:30:24 -0600

    update to include sa and sd

--- server/dfr3-service/src/main/java/edu/illinois/ncsa/incore/service/dfr3/utils/ValidationUtils.java
+++ server/dfr3-service/src/main/java/edu/illinois/ncsa/incore/service/dfr3/utils/ValidationUtils.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.regex.Pattern;
 
 public class ValidationUtils {
     // validate demand type matches demand unit or not
@@ -16,9 +17,28 @@ public class ValidationUtils {
 
         AtomicBoolean demandTypeExisted = new AtomicBoolean(false);
         AtomicBoolean demandUnitAllowed = new AtomicBoolean(false);
+
         listOfDemands.forEach(entry -> {
+            String demandTypeEvaluated = demandType;
+
             // check first if demand type exist
-            if (((JSONObject) entry).get("demand_type").toString().toLowerCase(Locale.ROOT).equals(demandType)) {
+            // sa and sd are special cases
+            if (demandType.contains("sa") || demandType.contains("sd")) {
+                String[] demandTypePhrase = demandType.split("\\s+");
+
+                // check if first word is positive numeric value
+                Pattern pattern = Pattern.compile("\\d+(\\.\\d+)?");
+                if (demandTypePhrase.length > 0 && demandTypePhrase.length < 3
+                    && pattern.matcher(demandTypePhrase[0]).matches()
+                    && (demandTypePhrase[demandTypePhrase.length - 1].equals("sa")
+                    || demandTypePhrase[demandTypePhrase.length - 1].equals("sd"))) {
+
+                    // replace demand type 0.1 sec Sa with just sa
+                    demandTypeEvaluated = demandTypePhrase[demandTypePhrase.length - 1];
+                }
+            }
+
+            if (((JSONObject) entry).get("demand_type").toString().toLowerCase(Locale.ROOT).equals(demandTypeEvaluated)) {
                 demandTypeExisted.set(true);
 
                 // check if demand unit is allowed
@@ -28,7 +48,10 @@ public class ValidationUtils {
                         demandUnitAllowed.set(true);
                     }
                 });
+
             }
+
+
         });
         return Arrays.asList(demandTypeExisted.get(), demandUnitAllowed.get());
     }
