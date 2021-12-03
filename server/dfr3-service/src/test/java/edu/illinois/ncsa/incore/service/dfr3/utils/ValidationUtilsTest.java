a5490a1af193a7fc15cc81cd0b1b8d34cc74a293
Chen Wang <cwang138@illinois.edu>
2021-12-02 18:09:46 -0600

    fix unit test

--- server/dfr3-service/src/test/java/edu/illinois/ncsa/incore/service/dfr3/utils/ValidationUtilsTest.java
+++ server/dfr3-service/src/test/java/edu/illinois/ncsa/incore/service/dfr3/utils/ValidationUtilsTest.java
@@ -5,10 +5,8 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 import static edu.illinois.ncsa.incore.service.dfr3.utils.ValidationUtils.isDemandValid;
@@ -19,16 +17,15 @@ public class ValidationUtilsTest {
     @Test
     public void testIsDemandValid() throws IOException {
 
-        URL path = this.getClass().getResource("demandDefinition.json");
-        InputStream is = new FileInputStream(String.valueOf(path));
+        InputStream is = this.getClass().getClassLoader().getResourceAsStream("demandDefinition.json");
         String jsonTxt = IOUtils.toString(is, StandardCharsets.UTF_8);
         JSONObject demandDefinition = new JSONObject(jsonTxt);
         JSONArray listOfDemands = demandDefinition.getJSONArray("earthquake");
 
-        assertTrue(isDemandValid("0.1 sec SA", "g", listOfDemands).get("demandTypeExisted"));
+        assertTrue(isDemandValid("0.1 sec sa", "g", listOfDemands).get("demandTypeExisted"));
         assertTrue(isDemandValid("0.1 sa", "g", listOfDemands).get("demandTypeExisted"));
         assertTrue(isDemandValid("sa", "g", listOfDemands).get("demandTypeExisted"));
-        assertFalse(isDemandValid("0.1 sec sec SA", "g", listOfDemands).get("demandTypeExisted"));
+        assertFalse(isDemandValid("0.1 sec sec sa", "g", listOfDemands).get("demandTypeExisted"));
         assertFalse(isDemandValid("0.1 sec", "g", listOfDemands).get("demandTypeExisted"));
         assertFalse(isDemandValid("sec sa", "g", listOfDemands).get("demandTypeExisted"));
     }
