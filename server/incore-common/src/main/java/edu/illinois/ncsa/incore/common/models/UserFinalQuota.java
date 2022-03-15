f7f1cf2d14a4bb96d6135ca70d590abcb31ac4f1
YONG WOOK KIM <ywkim@illinois.edu>
2022-03-15 11:24:58 -0500

    removed commented out lines

--- server/incore-common/src/main/java/edu/illinois/ncsa/incore/common/models/UserFinalQuota.java
+++ server/incore-common/src/main/java/edu/illinois/ncsa/incore/common/models/UserFinalQuota.java
@@ -34,12 +34,6 @@ public class UserFinalQuota {
     public UserFinalQuota() {
         this.applicationLimits = new UserUsages();
         this.username = null;
-//        this.datasets = AllocationConstants.NUM_DATASETS;
-//        this.hazards = AllocationConstants.NUM_HAZARDS;
-//        this.hazardDatasets = AllocationConstants.NUM_HAZARD_DATASETS;
-//        this.dfr3 = AllocationConstants.NUM_DFR3;
-//        this.datasetSize = AllocationConstants.DATASET_SIZE;
-//        this.hazardDatasetSize = AllocationConstants.HAZARD_DATASET_SIZE;
     }
 
     public String getId() {
@@ -52,97 +46,4 @@ public class UserFinalQuota {
 
     public void setApplicationLimits(UserUsages applicationLimits) { this.applicationLimits = applicationLimits; }
 
-//package edu.illinois.ncsa.incore.common.models;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import dev.morphia.annotations.Embedded;
-//import dev.morphia.annotations.Entity;
-//import edu.illinois.ncsa.incore.common.AllocationConstants;
-//import org.apache.log4j.Logger;
-//
-//import javax.xml.bind.annotation.XmlRootElement;
-//
-//@Embedded
-//public class UserFinalQuota {
-//    private static final Logger log = Logger.getLogger(UserFinalQuota.class);
-//
-//    public String userFinalQuotaId;
-//    public int datasets;
-//    public int hazards;
-//    public int hazardDatasets;
-//    public int dfr3;
-//    public long datasetSize;
-//    public long hazardDatasetSize;
-//
-//    public UserFinalQuota() {
-//        this.userFinalQuotaId = null;
-//        this.datasets = AllocationConstants.NUM_DATASETS;
-//        this.hazards = AllocationConstants.NUM_HAZARDS;
-//        this.hazardDatasets = AllocationConstants.NUM_HAZARD_DATASETS;
-//        this.dfr3 = AllocationConstants.NUM_DFR3;
-//        this.datasetSize = AllocationConstants.DATASET_SIZE;
-//        this.hazardDatasetSize = AllocationConstants.HAZARD_DATASET_SIZE;
-//    }
-//
-//    public static UserFinalQuota fromJson(String quotaJson) {
-//        try {
-//            ObjectMapper mapper = new ObjectMapper();
-//            return mapper.readValue(quotaJson, UserFinalQuota.class);
-//        } catch (Exception e) {
-//            log.error("Could not parse usage JSON. Returning Usage with zero values", e);
-//            return new UserFinalQuota();
-//        }
-//    }
-//
-//    public String getUserFinalQuotaId() { return this.userFinalQuotaId; }
-//
-//    public void setUserFinalQuotaId(String userFinalQuotaId) { this.userFinalQuotaId = userFinalQuotaId; }
-//
-//    public int getDatasets() {
-//        return this.datasets;
-//    }
-//
-//    public void setDatasets(int datasets) {
-//        this.datasets = datasets;
-//    }
-//
-//    public int getHazards() {
-//        return this.hazards;
-//    }
-//
-//    public void setHazards(int hazards) {
-//        this.hazards = hazards;
-//    }
-//
-//    public int getHazardDatasets() {
-//        return this.hazardDatasets;
-//    }
-//
-//    public void setHazardDatasets(int hazardDatasets) {
-//        this.hazardDatasets = hazardDatasets;
-//    }
-//
-//    public int getDfr3() {
-//        return this.dfr3;
-//    }
-//
-//    public void setDfr3(int dfr3) {
-//        this.dfr3 = dfr3;
-//    }
-//
-//    public long getDatasetSize() {
-//        return this.datasetSize;
-//    }
-//
-//    public void setDatasetSize(int datasetSize) {
-//        this.datasetSize = datasetSize;
-//    }
-//
-//    public long getHazardDatasetSize() {
-//        return this.hazardDatasetSize;
-//    }
-//
-//    public void setHazardDatasetSize(int hazardDatasetSize) {
-//        this.hazardDatasetSize = hazardDatasetSize;
-//    }
 }
