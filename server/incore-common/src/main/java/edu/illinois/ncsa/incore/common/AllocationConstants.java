ecd6335f01e66ab9c2dc375242063b1e49bd6831
YONG WOOK KIM <ywkim@illinois.edu>
2022-03-28 13:37:03 -0500

    added default quota to 10 times

--- server/incore-common/src/main/java/edu/illinois/ncsa/incore/common/AllocationConstants.java
+++ server/incore-common/src/main/java/edu/illinois/ncsa/incore/common/AllocationConstants.java
@@ -10,12 +10,12 @@
 package edu.illinois.ncsa.incore.common;
 
 public class AllocationConstants {
-    public static final int NUM_DATASETS = 100;
-    public static final int NUM_HAZARDS = 100;
+    public static final int NUM_DATASETS = 1000;
+    public static final int NUM_HAZARDS = 1000;
     public static final int NUM_HAZARD_DATASETS = 100;
-    public static final int NUM_DFR3 = 100;
-    public static final long DATASET_SIZE = 2147483648L;
-    public static final long HAZARD_DATASET_SIZE = 2147483648L;
+    public static final int NUM_DFR3 = 1000;
+    public static final long DATASET_SIZE = 21474836480L;
+    public static final long HAZARD_DATASET_SIZE = 21474836480L;
     public static final String DATASET_ALLOCATION_MESSAGE =
         "You have reached the maximum number of datasets you can create for your allocation. " +
             "Your request will be aborted. You either need to increase your allocation " +
