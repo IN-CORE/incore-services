fb09fa2784aee5a352dabe9ab8f90c55c37c8f1c
Ya-Lan Yang <ylyang@illinois.edu>
2024-01-23 14:38:26 -0600

    Not use sorting for now

--- server/semantics-service/src/main/java/edu/illinois/ncsa/incore/service/semantics/model/Type.java
+++ server/semantics-service/src/main/java/edu/illinois/ncsa/incore/service/semantics/model/Type.java
@@ -96,8 +96,7 @@ public class Type {
                     map.put("openvocab:versionnumber", this.getVersion());
 
                 } else if (field.getName().equals("tableSchema") && this.getTableSchema().getColumns() != null) {
-                    List<Map<String, Object>> updatedTableSchema = this.getTableSchema()
-                        .getSortedColumns("name", "asc").stream()
+                    List<Map<String, Object>> updatedTableSchema = this.getTableSchema().getColumns().stream()
                         .map(column -> column.constructOutput())
                         .collect(Collectors.toList());
                     Map<String, Object> columns = new HashMap<>();
