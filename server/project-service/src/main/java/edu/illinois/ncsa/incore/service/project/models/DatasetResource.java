85e45aef94a22c9632854b6fec098764b8235f89
Chen Wang <cwang138@illinois.edu>
2024-10-02 17:29:25 -0500

    add dataset object

--- server/project-service/src/main/java/edu/illinois/ncsa/incore/service/project/models/DatasetResource.java
+++ server/project-service/src/main/java/edu/illinois/ncsa/incore/service/project/models/DatasetResource.java
@@ -1,12 +1,32 @@
 package edu.illinois.ncsa.incore.service.project.models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import edu.illinois.ncsa.incore.common.data.models.jackson.JsonDateSerializer;
+
+import java.util.Date;
+import java.util.List;
 
 public class DatasetResource extends ProjectResource{
 
     // TODO Read from dataset object
     // @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+
     public String type;
+    public boolean deleted = false;
+    public String title = "";
+    public String description = "";
+    public Date date = new Date();
+    public String creator = null;
+    public String owner = null;
+    public List<String> contributors = null;
+    public List<FileDescriptor> fileDescriptors = null;
+    public String dataType = "";
+    public String storedUrl = "";
+    public String format = "";
+    public String sourceDataset = "";
+    public double[] boundingBox = null;
+    public List<String> spaces = null;
+    public NetworkDataset networkDataset = null;
 
     public DatasetResource() {
     }
@@ -16,5 +36,10 @@ public class DatasetResource extends ProjectResource{
     }
     public void setType(String type) {
         this.type = type;
+    }
+
+    @JsonSerialize(using = JsonDateSerializer.class)
+    public Date getDate() {
+        return date;
     }
 }
