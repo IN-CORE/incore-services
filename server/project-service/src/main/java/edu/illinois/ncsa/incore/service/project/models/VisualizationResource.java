fa4f85a8956fabe8cf3bfe26312faea2cf81e36a
Chen Wang <cwang138@illinois.edu>
2024-12-03 09:07:34 -0600

    remove unused import

--- server/project-service/src/main/java/edu/illinois/ncsa/incore/service/project/models/VisualizationResource.java
+++ server/project-service/src/main/java/edu/illinois/ncsa/incore/service/project/models/VisualizationResource.java
@@ -2,8 +2,6 @@ package edu.illinois.ncsa.incore.service.project.models;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import edu.illinois.ncsa.incore.common.data.models.jackson.JsonDateSerializer;
 
 import java.util.ArrayList;
 import java.util.List;
